ee5bba2924de27263a7c7f6d761ce6525025a328
cultofdeth <63765287+xsaze@users.noreply.github.com>
2025-11-08 02:45:40 +0200

    Update BottomBar.jsx

--- frontend/src/components/BottomBar.jsx
+++ frontend/src/components/BottomBar.jsx
@@ -57,7 +57,7 @@ export const BottomBar = ({ stats = {} }) => {
           <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
         </svg>
       </a>
-      <a href="#" className="social-link" title="Community" target="_blank"
+      <a href="https://x.com/i/communities/1986955894702137619" className="social-link" title="Community" target="_blank"
         rel="noopener noreferrer">
         <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
           <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z"/>
